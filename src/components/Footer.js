d988680b9376428f57a9594fbda2c9de0d34160b
Unknown <84217207+seaweedq12@users.noreply.github.com>
2021-10-19 13:35:19 +1100

    Adding readme to repo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const styles ={
 function Footer() {
   return (
     <div className="footer">
-      <ul className="d-flex flex-column flex-sm-row " >
+      <ul className="d-flex flex-column flex-md-row " >
         <li style={styles.style}><a className="fontup" href="mailto: aldurq99@gmail.com">Email: aldurq99@gmail.com</a></li>
         <li style={styles.style}><a className="fontup" href="https://github.com/seaweedq12" target="blank">Github</a></li>
         <li style={styles.style}><a className="fontup" href="https://www.linkedin.com/in/yeonseo-choi-a64691212/" target="blank">linkedIn</a></li>
